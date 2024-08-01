331275c55469ed2c823d5ce0b67e30bca7a3bca7
Jimi-Chhatrala <jimichhatrala8488@gmail.com>
2024-08-01 17:20:37 +0530

    REST API File CRUD

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json");
+const fs = require("fs");
+const { error } = require("console");
+const port = 8000;
 
 const app = express();
+
+// Middleware - Plugin
+app.use(express.urlencoded({ extended: false }));
 
 // Routes
 
@@ -24,9 +30,18 @@ app.get("/api/users", (req, res) => {
   return res.json(users);
 });
 
+// Add a new user
 app.post("/api/users", (req, res) => {
-  // TODO: Create new user
-  return res.json({ status: "Pending" });
+  const body = req.body;
+  const newUser = { ...body, id: users.length + 1 };
+  users.push(newUser);
+
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
+    if (err) {
+      return res.status(500).json({ status: "Failed to add user", error: err });
+    }
+    return res.json({ status: "New user added.", id: newUser.id });
+  });
 });
 
 app
@@ -34,19 +49,71 @@ app
   .get((req, res) => {
     const id = Number(req.params.id);
     const user = users.find((user) => user.id === id);
+    if (!user) {
+      return res.status(404).json({ status: "User not found" });
+    }
     return res.json(user);
   })
   .patch((req, res) => {
-    // TODO: Edit partial info of the user  with id
-    return res.json({ status: "Pending" });
+    const id = Number(req.params.id);
+    const userIndex = users.findIndex((user) => user.id === id);
+
+    if (userIndex === -1) {
+      return res.status(404).json({ status: "User not found" });
+    }
+
+    // Merge the existing user with the new data (partial update)
+    users[userIndex] = { ...users[userIndex], ...req.body };
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
+      if (err) {
+        return res
+          .status(500)
+          .json({ status: "Failed to update user", error: err });
+      }
+
+      return res.json({ status: "User updated", user: users[userIndex] });
+    });
   })
   .put((req, res) => {
-    // TODO: Edit whole info of the user  with id
-    return res.json({ status: "Pending" });
+    const id = Number(req.params.id);
+    const userIndex = users.findIndex((user) => user.id === id);
+
+    if (userIndex === -1) {
+      return res.status(404).json({ status: "User not found" });
+    }
+
+    // Completely replace the user object
+    users[userIndex] = { ...req.body, id };
+
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
+      if (err) {
+        return res
+          .status(500)
+          .json({ status: "Failed to update user", error: err });
+      }
+      return res.json({ status: "User fully updated", user: users[userIndex] });
+    });
   })
   .delete((req, res) => {
-    // TODO: Delete the user with id
-    return res.json({ status: "Pending" });
+    const id = Number(req.params.id);
+    console.log(id);
+    const userIndex = users.findIndex((user) => user.id === id);
+    console.log(userIndex);
+    if (userIndex === -1) {
+      return res.status(404).json({ status: "User not found" });
+    }
+
+    // Remove the user from the array
+    const deletedUser = users.splice(userIndex, 1)[0];
+
+    fs.writeFile("./MOCK_DAT.json", JSON.stringify(users), (err) => {
+      if (err) {
+        return res
+          .status(500)
+          .json({ status: "Failed to delete user", error: err });
+      }
+      return res.json({ status: "User deleted", user: deletedUser });
+    });
   });
 
 app.get("/about", (req, res) => {
